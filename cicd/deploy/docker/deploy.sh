ee561f6105d3ee459f1f20a0a188cf46505cc2a2
zhanchenjin <zhanchenjin@magefitness.com>
2021-01-30 18:18:28 +0800

    + 增加版本号

--- cicd/deploy/docker/deploy.sh
+++ cicd/deploy/docker/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/app
-docker build -t beegi_test -f /var/app/beego_test/cicd/deploy/docker/Dockerfile .
+docker build -t beegi_test:1.0.0 -f /var/app/beego_test/cicd/deploy/docker/Dockerfile .
