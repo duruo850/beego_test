6ff3142ed57bb7ed44a7608dc633d53b1b55a342
zhanchenjin <zhanchenjin@magefitness.com>
2021-02-01 10:28:38 +0800

    + 增加build，start方法

--- cicd/deploy/docker/deploy.sh
+++ cicd/deploy/docker/deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-cd /var/app
-docker build -t beegi_test:1.0.0 -f /var/app/beego_test/cicd/deploy/docker/Dockerfile .
 
-# sudo docker run -it --network default_network -p 0.0.0.0:8900:8900 beegi_test:1.0.0 /bin/bash
+
+build() {
+    echo "build..."
+    cd /var/app
+    docker build -t beegi_test:1.0.0 -f /var/app/beego_test/cicd/deploy/docker/Dockerfile .
+}
+
+start() {
+    echo "start..."
+    sudo docker run --name beege_test -it --network default_network -d --restart=always -p 0.0.0.0:8900:8900 beegi_test:1.0.0
+}
+
+
+cmd=$1
+case $cmd in
+build)
+        build
+;;
+start)
+        start
+;;
+esac
+exit 0
